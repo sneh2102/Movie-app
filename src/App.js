e9a11249d1d88911b5037a3b0a0510a385b7ebd6
sneh2102 <patel.sneh2102@gmail.com>
2023-03-27 09:05:46 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,78 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
 
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import MainMovie from './components/MainMovie';
-import SingleMovie from './components/SingleMovie'
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
-const App = () => {
+function App() {
+  const [movies, setMovies] = useState([]);
+  const [selectedMovie, setSelectedMovie] = useState(null);
+  const [searchTerm, setSearchTerm] = useState("");
+
+
+  useEffect(() => {
+	if(searchTerm===""){
 	
-	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path='/' element={<MainMovie/>}/>
-				<Route path="movie/:id" element={<SingleMovie/>} />
-			</Routes>
-		</BrowserRouter>
-	);
-};
+    fetch(
+      `https://api.themoviedb.org/3/movie/popular?api_key=1cfcd011743e20a4ccd560246fcb6e3f`
+    )
+      .then((response) => response.json())
+      .then((data) => setMovies(data.results))
+      .catch((error) => console.log(error));
+	  console.log(movies);
+  }
+  else{	
+    fetch(
+		`https://api.themoviedb.org/3/search/movie?api_key=1cfcd011743e20a4ccd560246fcb6e3f&language=en-US&query=${searchTerm}&page=1&include_adult=false`
+	  )
+		.then((response) => response.json())
+		.then((data) => setMovies(data.results))
+		.catch((error) => console.log(error));
+		console.log(movies);
+  }}, [searchTerm]);
+
+  const handleClick = (movie) => {
+    setSelectedMovie(movie);
+  };
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  }
+
+  return (
+    <div className="App">
+      <header>
+        <h1>Movies App</h1>
+        <input  type="text"
+          placeholder="Search movies..."
+          value={searchTerm}
+          onChange={handleSearch} />
+      </header>
+      {selectedMovie && (
+        <div className="details">
+          <img
+            src={`https://image.tmdb.org/t/p/w500/${selectedMovie.poster_path}`}
+            alt={selectedMovie.title}
+          />
+          <h2>{selectedMovie.title}</h2>
+          <p>{selectedMovie.overview}</p>
+          <div className="close-button" onClick={() => setSelectedMovie(null)}></div>
+        </div>
+      )}
+      <main>
+        {movies.map((movie) => (
+          <div key={movie.id} className="movie-container">
+			<div className="movie-card">
+            <img
+              src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+              alt={movie.title}
+              onClick={() => handleClick(movie)}
+            />
+            <h2>{movie.title}</h2>
+			<div className="rating">Rating: {movie.vote_average}</div>
+			</div>
+          </div>
+        ))}
+      </main>
+    </div>
+  );
+}
 
 export default App;
--- src/store.js
+++
@@ -1,8 +0,0 @@
-import rootReducer from "./reducers/index";
-import  {legacy_createStore, applyMiddleware,compose }  from "redux";
-
-const store = legacy_createStore(rootReducer,compose(
-    applyMiddleware(ReduxThunk)
-    )
-);
-export default store;
